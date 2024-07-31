4eccbf304fd2f686db925175fbe3879549a6090c
Kubilay Arslan <kubilay.arslan@outlook.com.tr>
2024-07-31 23:13:15 +0300

    TASK TO-DO COMPOSE-02: Implement database layer
    Description:
    - Database logic created.

---
+++ app/src/main/java/com/example/to_docompose/data/ToDoDatabase.kt
@@ -0,0 +1,10 @@
+package com.example.to_docompose.data
+
+import androidx.room.Database
+import com.example.to_docompose.data.models.ToDoTask
+
+@Database(entities = [ToDoTask::class], version = 1, exportSchema = false)
+abstract class ToDoDatabase {
+
+    abstract fun toDoDao(): ToDoDAO
+}
